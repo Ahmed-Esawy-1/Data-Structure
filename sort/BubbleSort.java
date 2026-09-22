230a02eaa3d234a36d15e6183d2b080f2583c953
Ahmed Esawy <ahmed76esawy@gmail.com>
2026-09-22 17:53:41 +0300

    Bubble Sort Imp

---
+++ sort/BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int nums[] = { 6, 5, 2, 8, 9, 4 };
+        int size = nums.length;
+        int temp = 0;
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println("<- Before Sorting");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println("<- After Sorting");
+
+    }
+}
