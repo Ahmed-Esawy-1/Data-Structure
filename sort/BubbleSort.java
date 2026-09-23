67c8c6872c4744ba8f97abf19882bffb6e21f68d
Ahmed Esawy <ahmed76esawy@gmail.com>
2026-09-23 15:49:04 +0300

    Change the array values

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -1,29 +1,36 @@
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int nums[] = { 6, 5, 2, 8, 9, 4 };
-        int size = nums.length;
-        int temp = 0;
+        int nums[] = { 5, 3, 4, 8, 2 };
 
         for (int num : nums) {
             System.out.print(num + " ");
         }
         System.out.println("<- Before Sorting");
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
-        }
+        nums = bubbleSort(nums);
 
         for (int num : nums) {
             System.out.print(num + " ");
         }
         System.out.println("<- After Sorting");
+    }
 
+    // ---- FUNCTION
+    public static int[] bubbleSort(int arr[]) {
+        int size = arr.length;
+        int temp = 0;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        return arr;
     }
 }
